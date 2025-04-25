dee3093241e11bd7a2b673f1e7dd6d4091929c04
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2025-04-25 20:08:19 +0530

    configure model mapper

--- src/main/java/com/example/question/QuestionApplication.java
+++ src/main/java/com/example/question/QuestionApplication.java
@@ -1,13 +1,21 @@
 package com.example.question;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuestionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionApplication.class, args);
+	}
+
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
